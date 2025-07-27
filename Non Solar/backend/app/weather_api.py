22a3ecd8924a800e66fdb7431c76aec83b50d3ad
Mohamed Ali <mohamed.vdk87@gmail.com>
2025-07-27 10:34:53 +0530

    Initial Commit

--- Non Solar/backend/app/weather_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def fetch_live_weather(api_key, city='Chennai'):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    return response.json()
